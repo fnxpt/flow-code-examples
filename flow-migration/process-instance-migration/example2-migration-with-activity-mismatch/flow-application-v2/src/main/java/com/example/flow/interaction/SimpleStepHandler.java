6f4a00ada65a91c3e10b3e3db089509a89927e62
Andre Mare <andrem@backbase.com>
2020-10-10 18:52:53 +0200

    Update Example Layout

---
+++ flow-migration/process-instance-migration/example2-migration-with-activity-mismatch/flow-application-v2/src/main/java/com/example/flow/interaction/SimpleStepHandler.java
@@ -0,0 +1,48 @@
+package com.example.flow.interaction;
+
+import com.backbase.flow.casedata.CaseDataService;
+import com.backbase.flow.interaction.engine.action.ActionHandler;
+import com.backbase.flow.interaction.engine.action.ActionResult;
+import com.backbase.flow.interaction.engine.action.InteractionContext;
+import com.example.flow.casedata.CaseDefinition;
+import com.example.flow.interaction.SimpleStepHandler.SimpleStepHandlerResponse;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.camunda.bpm.engine.runtime.MessageCorrelationResult;
+import org.springframework.stereotype.Component;
+
+@Component("simple-step-handler")
+@RequiredArgsConstructor
+public class SimpleStepHandler implements ActionHandler<CaseDefinition, SimpleStepHandlerResponse> {
+
+    private final CaseDataService caseDataService;
+
+    @Override
+    public ActionResult<SimpleStepHandlerResponse> handle(CaseDefinition simpleCaseDefinition,
+        InteractionContext interactionContext) {
+
+        interactionContext.saveCaseData(simpleCaseDefinition);
+
+        String processId = interactionContext.startProcess(interactionContext.getCaseKey(), "order-pizza");
+
+        simpleCaseDefinition.setStartedProcess(true);
+
+        interactionContext.saveCaseData(simpleCaseDefinition);
+
+        return new ActionResult<>(new SimpleStepHandlerResponse(interactionContext.getCaseKey(), processId));
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class SimpleStepHandlerResponse {
+
+        private UUID caseKey;
+        private String processId;
+    }
+}
